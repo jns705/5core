994dd84fe6e2859157c2869c8e7ee419eb6ab0b8
paruko79@gmail.com <paruko79@gmail.com>
2025-11-24 16:12:08 +0900

    구매상담 화면 호출 Controller에 추가

--- src/main/java/com/core/controller/CounselingController.java
+++ src/main/java/com/core/controller/CounselingController.java
@@ -1,5 +1,18 @@
 package com.core.controller;
 
-public class CounselingController {
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Controller
+public class CounselingController {
+	
+	@GetMapping("/counseling")
+	public String requestCounseling(Model model) {
+		return "counseling/counseling";
+		
+	}
 }
