80881d315f94d9e5972e72a6c64e841af0a55ab0
DESKTOP-IOJJSJV\USER <jns705@naver.com>
2025-11-24 15:12:20 +0900

    메인

--- src/main/java/com/core/controller/CounselingController.java
+++ src/main/java/com/core/controller/CounselingController.java
@@ -1,5 +1,21 @@
 package com.core.controller;
 
-public class CounselingController {
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Controller
+@RequestMapping("/counseling")
+public class CounselingController {
+	
+	@GetMapping("/add")
+	public String requestMain(Model model) {
+		return "main";
+	}
+	
 }
