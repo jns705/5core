55a4364ab5ea5d969e0c08dc13a48808025a252e
char <ms06jaku1@gmail.com>
2025-11-24 16:42:28 +0900

    페이지 통합 추가

--- src/main/java/com/core/controller/CounselingController.java
+++ src/main/java/com/core/controller/CounselingController.java
@@ -2,17 +2,24 @@ package com.core.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @Controller
+@RequestMapping("/counseling")
 public class CounselingController {
 
     // 상담 글 단건 조회
-    @GetMapping("/counseling")
+    @GetMapping
     public String counseling() {
         return "counseling/counseling";
     }
+
+	@GetMapping("/detail")
+	public String counselingDetail() {
+	    return "counseling/counselingDetail";
+}
 }
     
