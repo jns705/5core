62a370237b9cbe6334ce42698876c079bcb51baf
paruko79@gmail.com <paruko79@gmail.com>
2025-12-05 18:09:35 +0900

    포트번호 변경및 그외 수정

--- src/main/java/com/core/config/StreamlitRunner.java
+++ src/main/java/com/core/config/StreamlitRunner.java
@@ -53,7 +53,8 @@ public class StreamlitRunner implements ApplicationRunner {
                 "--server.port=8501"
         );
 		
-		ProcessBuilder builder2 = new ProcessBuilder(
+        // applyDetailRecommend: 상담 상세 차량 추천 (8502)
+		ProcessBuilder applyDetailRecommend = new ProcessBuilder(
                 pythonPath.toString(),
                 "-m", "streamlit", "run",
                 "applyDetail.py",
@@ -61,25 +62,25 @@ public class StreamlitRunner implements ApplicationRunner {
                 "--server.port=8502"
         );
 		
-        // builder2: 판매 실적 그래프 (8502)
+        // dealerSaleGraph: 판매 실적 그래프 (8503)
 		ProcessBuilder dealerSaleGraph = new ProcessBuilder(
 		    pythonPath.toString(), "-m", "streamlit", "run",
 		    "sales_graph.py",
 		    "--server.baseUrlPath=5core",
-		    "--server.port=8502"
+		    "--server.port=8503"
 		);
 		
 		// 실행 디렉토리
 		builder.directory(aiServerDir.toFile());
-		builder2.directory(aiServerDir.toFile());
+		applyDetailRecommend.directory(aiServerDir.toFile());
 		dealerSaleGraph.directory(aiServerDir.toFile());
 		
 		// 프로세스 실행
 		// - 실행되었다면 console에 streamlit이 실행되었다고 뜸
 		builder.start();
-		builder2.start();
-		System.out.println("Streamlit이 실행됨");
+		applyDetailRecommend.start();
 		dealerSaleGraph.start();
+		System.out.println("Streamlit이 실행됨");
 		System.out.println("판매 실적 그래프(딜러) 실행됨");
 	}
 	
